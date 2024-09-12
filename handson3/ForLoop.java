ac98ea4a677df4bcc82a07804115ae04f28a3668
BenayaRIzkySipayung <2381045@unai.edu>
2024-09-12 22:12:34 +0700

    Handson3

---
+++ handson3/ForLoop.java
@@ -0,0 +1,16 @@
+package handson3;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            if (i % 3 == 0 && i != 15) {
+                continue;
+
+            }
+            System.out.println("Nilai i : " + i);
+            if (i == 15) {
+                break;
+            }
+        }
+    }
+}
